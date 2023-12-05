def6eb0ed30de315f2fa5b40378280256573209d
Mahesh-Chaulagain <maheshchaulagain8@gmail.com>
2023-12-05 21:30:57 +0545

    generated the hex color for the background

--- app.js
+++ app.js
@@ -5,9 +5,16 @@ const color = document.querySelector(".color");
 
 btn.addEventListener("click", function(){
     // get random number between 0 and 3
-    const randomNumber = 2;
+    const randomNumber = getRandomNumber();
+    console.log(randomNumber);
     // style the body background
     document.body.style.backgroundColor = colors[randomNumber];
-    // change the text content of color with new color name
+    // change the text content of class "color" with new color name
     color.textContent = colors[randomNumber];
-})
+});
+
+function getRandomNumber(){
+    // get random number from 0 all the way to 1 using Math.random()
+    // to round down the number to the closest integer use Math.floor()
+    return Math.floor(Math.random() * colors.length);
+}
